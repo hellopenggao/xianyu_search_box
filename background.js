7c3a772486316c7327e04ebbc4ceea4996029f87
peng.gao <peng.gao@yuneec.com>
2018-12-03 21:27:09 +0800

    点击图标进入闲鱼搜索页

--- background.js
+++ background.js
@@ -1,4 +1,4 @@
 chrome.browserAction.onClicked.addListener(function (tab) {
-    chrome.tabs.create({'url': "https://2.taobao.com"}, function (tab) {
+    chrome.tabs.create({'url': "https://s.2.taobao.com/list/list.htm?q=%CA%D6%BB%FA&search_type=item&app=shopsearch"}, function (tab) {
     })
 })
